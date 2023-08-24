05c63b71b3e0a6d8037b0c62d9d8ff0b260bb0fe
Blockchainmaster <blockchainmaster2022@gmail.com>
2023-08-25 00:42:32 +0900

    storage change

--- util/multer.js
+++ util/multer.js
@@ -1,7 +1,28 @@
 const multer = require("multer");
 const fs = require("fs");
 
+// module.exports = multer.diskStorage({
+//   filename: (req, file, callback) => {
+//     const filename =
+//       Date.now() + Math.floor(Math.random() * 100) + file.originalname.replace(/ /g, "");
+//     callback(null, filename);
+//   },
+//   destination: (req, file, callback) => {
+//     if (!fs.existsSync("storage")) {
+//       fs.mkdirSync("storage");
+//     }
+//     callback(null, "storage");
+//   },
+// });
+
 module.exports = multer.diskStorage({
+  sftp: {
+    host: 'http://ftp.lukra.net/',
+    port: 21,
+    username: 'rayne@geniu.app',
+    password: 'iKPJWdt68ch2hYk'
+
+  },
   filename: (req, file, callback) => {
     const filename =
       Date.now() + Math.floor(Math.random() * 100) + file.originalname.replace(/ /g, "");
