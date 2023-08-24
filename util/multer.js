ba4e9171b51679f5adbcc2e03599d964069bc893
Blockchainmaster <blockchainmaster2022@gmail.com>
2023-08-25 01:16:34 +0900

    storage ftp

--- util/multer.js
+++ util/multer.js
@@ -1,5 +1,6 @@
 const multer = require("multer");
 const fs = require("fs");
+const FTPStorage = require('multer-ftp-storage');
 
 // module.exports = multer.diskStorage({
 //   filename: (req, file, callback) => {
@@ -15,23 +16,11 @@ const fs = require("fs");
 //   },
 // });
 
-module.exports = multer.diskStorage({
-  sftp: {
-    host: 'http://ftp.lukra.net/',
-    port: 21,
-    username: 'rayne@geniu.app',
-    password: 'iKPJWdt68ch2hYk'
-
-  },
-  filename: (req, file, callback) => {
-    const filename =
-      Date.now() + Math.floor(Math.random() * 100) + file.originalname.replace(/ /g, "");
-    callback(null, filename);
-  },
-  destination: (req, file, callback) => {
-    if (!fs.existsSync("storage")) {
-      fs.mkdirSync("storage");
-    }
-    callback(null, "storage");
+module.exports = new FTPStorage({
+  ftp_config: {
+    host: "http://ftp.lukra.net/",
+    secure: false, // enables FTPS/FTP with TLS
+    user: "rayne@geniu.app",
+    password: "iKPJWdt68ch2hYk",
   },
 });
